ad63bc6d863018a2052b609a5e89cc1fad780a68
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-11 10:29:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ document.body.appendChild(p);
 // create element from script
 if (document.getElementById("fot").innerHTML.split("*")[1] == "button") {
 var text = document.createElement("button");
-text.innerHTML = document.getElementById("fot").innerHTML.split("\")[1];
+text.innerHTML = document.getElementById("fot").innerHTML.split("$")[1];
 text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "text") {
 var text = document.createElement("p");
-text.innerHTML = document.getElementById("fot").innerHTML.split("\")[1];
+text.innerHTML = document.getElementById("fot").innerHTML.split("$")[1];
 text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "heading") {
 var text = document.createElement("h2");
-text.innerHTML = document.getElementById("fot").innerHTML.split("\")[1];
+text.innerHTML = document.getElementById("fot").innerHTML.split("$")[1];
 text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "input") {
@@ -31,7 +31,7 @@ text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "link") {
 var text = document.createElement("a");
-text.innerHTML = document.getElementById("fot").innerHTML.split("\")[1];
+text.innerHTML = document.getElementById("fot").innerHTML.split("$")[1];
 text.href = document.getElementById("fot").innerHTML.split("/url/")[1];
 text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
@@ -60,7 +60,7 @@ text.style.width = document.getElementById("fot").innerHTML.split("/width/")[1];
 text.id = document.getElementById("fot").innerHTML.split("#")[1];
 div.appendChild(text);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "alert") {
-alert(document.getElementById("fot").innerHTML.split("\")[1]);
+alert(document.getElementById("fot").innerHTML.split("$")[1]);
 } else if (document.getElementById("fot").innerHTML.split("*")[1] == "embed") {
 var text = document.createElement("embed");
 text.src = document.getElementById("fot").innerHTML.split("/src/")[1];
@@ -77,17 +77,17 @@ div.appendChild(text);
 // second line of code
 if (document.getElementById("fot").innerHTML.split("*")[3] == "button") {
 var three = document.createElement("button");
-three.innerHTML = document.getElementById("fot").innerHTML.split("\")[3];
+three.innerHTML = document.getElementById("fot").innerHTML.split("$")[3];
 three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
 } else if (document.getElementById("fot").innerHTML.split("*")[3] == "text") {
 var three = document.createElement("p");
-three.innerHTML = document.getElementById("fot").innerHTML.split("\")[3];
+three.innerHTML = document.getElementById("fot").innerHTML.split("$")[3];
 three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
 } else if (document.getElementById("fot").innerHTML.split("*")[3] == "heading") {
 var three = document.createElement("h2");
-three.innerHTML = document.getElementById("fot").innerHTML.split("\")[3];
+three.innerHTML = document.getElementById("fot").innerHTML.split("$")[3];
 three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
 } else if (document.getElementById("fot").innerHTML.split("*")[3] == "input") {
@@ -97,7 +97,7 @@ three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
 } else if (document.getElementById("fot").innerHTML.split("*")[3] == "link") {
 var three = document.createElement("a");
-three.innerHTML = document.getElementById("fot").innerHTML.split("\")[3];
+three.innerHTML = document.getElementById("fot").innerHTML.split("$")[3];
 three.href = document.getElementById("fot").innerHTML.split("/url/")[3];
 three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
@@ -120,22 +120,22 @@ three.style.width = document.getElementById("fot").innerHTML.split("/width/")[3]
 three.id = document.getElementById("fot").innerHTML.split("#")[3];
 div.appendChild(three);
 } else if (document.getElementById("fot").innerHTML.split("*")[3] == "alert") {
-alert(document.getElementById("fot").innerHTML.split("\")[3]);
+alert(document.getElementById("fot").innerHTML.split("$")[3]);
 }
 // third line of code
 if (document.getElementById("fot").innerHTML.split("*")[5] == "button") {
 var five = document.createElement("button");
-five.innerHTML = document.getElementById("fot").innerHTML.split("\")[5];
+five.innerHTML = document.getElementById("fot").innerHTML.split("$")[5];
 five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
 } else if (document.getElementById("fot").innerHTML.split("*")[5] == "text") {
 var five = document.createElement("p");
-five.innerHTML = document.getElementById("fot").innerHTML.split("\")[5];
+five.innerHTML = document.getElementById("fot").innerHTML.split("$")[5];
 five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
 } else if (document.getElementById("fot").innerHTML.split("*")[5] == "heading") {
 var five = document.createElement("h2");
-five.innerHTML = document.getElementById("fot").innerHTML.split("\")[5];
+five.innerHTML = document.getElementById("fot").innerHTML.split("$")[5];
 five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
 } else if (document.getElementById("fot").innerHTML.split("*")[5] == "input") {
@@ -145,7 +145,7 @@ five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
 } else if (document.getElementById("fot").innerHTML.split("*")[5] == "link") {
 var five = document.createElement("a");
-five.innerHTML = document.getElementById("fot").innerHTML.split("\")[5];
+five.innerHTML = document.getElementById("fot").innerHTML.split("$")[5];
 five.href = document.getElementById("fot").innerHTML.split("/url/")[5];
 five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
@@ -168,22 +168,22 @@ five.style.width = document.getElementById("fot").innerHTML.split("/width/")[5];
 five.id = document.getElementById("fot").innerHTML.split("#")[5];
 div.appendChild(five);
 } else if (document.getElementById("fot").innerHTML.split("*")[5] == "alert") {
-alert(document.getElementById("fot").innerHTML.split("\")[5]);
+alert(document.getElementById("fot").innerHTML.split("$")[5]);
 }
 //fouth line of code
 if (document.getElementById("fot").innerHTML.split("*")[7] == "button") {
 var seven = document.createElement("button");
-seven.innerHTML = document.getElementById("fot").innerHTML.split("\")[7];
+seven.innerHTML = document.getElementById("fot").innerHTML.split("$")[7];
 seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
 } else if (document.getElementById("fot").innerHTML.split("*")[7] == "text") {
 var seven = document.createElement("p");
-seven.innerHTML = document.getElementById("fot").innerHTML.split("\")[7];
+seven.innerHTML = document.getElementById("fot").innerHTML.split("$")[7];
 seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
 } else if (document.getElementById("fot").innerHTML.split("*")[7] == "heading") {
 var seven = document.createElement("h2");
-seven.innerHTML = document.getElementById("fot").innerHTML.split("\")[7];
+seven.innerHTML = document.getElementById("fot").innerHTML.split("$")[7];
 seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
 } else if (document.getElementById("fot").innerHTML.split("*")[7] == "input") {
@@ -193,7 +193,7 @@ seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
 } else if (document.getElementById("fot").innerHTML.split("*")[7] == "link") {
 var seven = document.createElement("a");
-seven.innerHTML = document.getElementById("fot").innerHTML.split("\")[7];
+seven.innerHTML = document.getElementById("fot").innerHTML.split("$")[7];
 seven.href = document.getElementById("fot").innerHTML.split("/url/")[7];
 seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
@@ -216,22 +216,22 @@ seven.style.width = document.getElementById("fot").innerHTML.split("/width/")[7]
 seven.id = document.getElementById("fot").innerHTML.split("#")[7];
 div.appendChild(seven);
 } else if (document.getElementById("fot").innerHTML.split("*")[7] == "alert") {
-alert(document.getElementById("fot").innerHTML.split("\")[7]);
+alert(document.getElementById("fot").innerHTML.split("$")[7]);
 }
 // fifth line of code
 if (document.getElementById("fot").innerHTML.split("*")[9] == "button") {
 var nine = document.createElement("button");
-nine.innerHTML = document.getElementById("fot").innerHTML.split("\")[9];
+nine.innerHTML = document.getElementById("fot").innerHTML.split("$")[9];
 nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
 } else if (document.getElementById("fot").innerHTML.split("*")[9] == "text") {
 var nine = document.createElement("p");
-nine.innerHTML = document.getElementById("fot").innerHTML.split("\")[9];
+nine.innerHTML = document.getElementById("fot").innerHTML.split("$")[9];
 nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
 } else if (document.getElementById("fot").innerHTML.split("*")[9] == "heading") {
 var nine = document.createElement("h2");
-nine.innerHTML = document.getElementById("fot").innerHTML.split("\")[9];
+nine.innerHTML = document.getElementById("fot").innerHTML.split("$")[9];
 nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
 } else if (document.getElementById("fot").innerHTML.split("*")[9] == "input") {
@@ -241,7 +241,7 @@ nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
 } else if (document.getElementById("fot").innerHTML.split("*")[9] == "link") {
 var nine = document.createElement("a");
-nine.innerHTML = document.getElementById("fot").innerHTML.split("\")[9];
+nine.innerHTML = document.getElementById("fot").innerHTML.split("$")[9];
 nine.href = document.getElementById("fot").innerHTML.split("/url/")[9];
 nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
@@ -264,6 +264,6 @@ nine.style.width = document.getElementById("fot").innerHTML.split("/width/")[9];
 nine.id = document.getElementById("fot").innerHTML.split("#")[9];
 div.appendChild(nine);
 } else if (document.getElementById("fot").innerHTML.split("*")[9] == "alert") {
-alert(document.getElementById("fot").innerHTML.split("\")[9]);
+alert(document.getElementById("fot").innerHTML.split("$")[9]);
 }
 }
