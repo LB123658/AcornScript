6fb5da68f586cc5c190610cf6746a22df4709396
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-25 08:36:50 -0500

    Create run.js

---
+++ run.js
@@ -0,0 +1,17 @@
+function load() {
+  document.getElementById("txt").innerHTML = localStorage.getItem("acornscript");
+  myFunction();
+}
+function run() {
+// Check browser support
+var input = document.getElementById("txt");
+if (typeof(Storage) !== "undefined") {
+  // Store
+  localStorage.setItem("notebook", input.value);
+  // Retrieve
+  document.getElementById("txt").innerHTML = localStorage.getItem("acornscript");
+} else {
+  alert("Your browser does not allow this note to be saved");
+}
+  location.reload();
+}
