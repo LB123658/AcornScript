cbb0b493dd1afc5372e5d1150289cd04191ad21e
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-25 08:50:58 -0500

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ function load() {
   document.getElementById("txt").innerHTML = localStorage.getItem("acornscript");
 }
 function run() {
-  var txt = document.GetElementById("txt");
+  var txt = document.getElementById("txt");
 // Check browser support
 if (typeof(Storage) !== "undefined") {
   // Store
