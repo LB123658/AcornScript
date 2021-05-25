b134069d339ab87f49392becd107e23edebadb5e
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-25 08:42:17 -0500

    Update run.js

--- run.js
+++ run.js
@@ -4,14 +4,13 @@ function load() {
 }
 function run() {
 // Check browser support
-var input = document.getElementById("txt");
 if (typeof(Storage) !== "undefined") {
   // Store
-  localStorage.setItem("acornscript", input.value);
+  localStorage.setItem("acornscript", document.getElementById("txt").value);
   // Retrieve
   document.getElementById("txt").innerHTML = localStorage.getItem("acornscript");
 } else {
-  alert("Your browser does not allow this note to be saved");
+  alert("Your browser does not allow this code to be run. Turn on local storage.");
 }
   location.reload();
 }
